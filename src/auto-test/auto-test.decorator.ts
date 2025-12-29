a0e4d28d27ba706a14b4d90a796a3426eeba8ba6
aasemikov <aleksey.i99@mail.ru>
2025-12-29 14:15:13 +0300

    Изменена логика на работу через контроллер

--- src/auto-test/auto-test.decorator.ts
+++ src/auto-test/auto-test.decorator.ts
@@ -1,14 +1,9 @@
 import { SetMetadata } from '@nestjs/common';
-import { IAutoTestMetadata } from '../interfaces/auto-test-metadata.interface';
 import { AUTO_TEST_METADATA } from './auto-test.constants';
 
 /**
- * Помечает GET-эндпоинт для автоматического тестирования.
- * @param path Относительный путь к эндпоинту (например, '/health')
+ * Помечает весь контроллер для автоматического тестирования всех GET-эндпоинтов.
  */
-export const AutoTest = (path: string): MethodDecorator => {
-    return SetMetadata(AUTO_TEST_METADATA, {
-        path,
-        method: 'GET',
-    } as IAutoTestMetadata);
+export const AutoTest = (): ClassDecorator => {
+    return SetMetadata(AUTO_TEST_METADATA, true);
 };
--- src/interfaces/auto-test-metadata.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IAutoTestMetadata {
-    path: string;
-    method: 'GET';
-    controller: unknown;
-    methodName: string;
-}
